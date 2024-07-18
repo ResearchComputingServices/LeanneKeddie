eb08eb01e281102bbd79c76ee17144e6cde3b67f
Nicholi Shiell <nicholishiell@gmail.com>
2024-07-18 15:55:20 -0400

    single page display

--- webapp/add_data_page.py
+++ webapp/add_data_page.py
@@ -8,13 +8,39 @@ from PDFHighlighter import PDFHighlighter
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-def select_file_cb(pdf_file_name : str) -> None:
+def select_file_cb(proxy_statement_name : str) -> None:
+    
+    st.session_state[ACTIVE_PAGE_NUMBER_KEY] = 0  
+    st.session_state[ACTIVE_PROXY_STATEMENT_NAME_KEY] = proxy_statement_name
+    
+    update_displayed_pdf()
 
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+def get_active_proxy_statement_pdf_file_name() -> str:
+    
+    num_leading_zeros = 4 - len(str(st.session_state[ACTIVE_PAGE_NUMBER_KEY] + 1))
+    
+    page_num = st.session_state[ACTIVE_PAGE_NUMBER_KEY]
+    
+    active_proxy_statement_name = st.session_state[ACTIVE_PROXY_STATEMENT_NAME_KEY]
+    active_pdf_dir_path = st.session_state[PROXY_STATEMENTS_KEY][active_proxy_statement_name] 
+    
+    active_pdf_file_path = f"{active_pdf_dir_path}/page_{'0'*num_leading_zeros}{page_num + 1}.pdf"
+        
+    return active_pdf_file_path
+
+def update_displayed_pdf() -> None:
+
+    pdf_file_name = get_active_proxy_statement_pdf_file_name()
+    print(f'Active pdf: {pdf_file_name}')
+    
     # create a tmp file for it proxy statement which will be highlighted      
-    if create_tmp_file(pdf_file_name):
+    if create_tmp_file():
     
         # create a PDFHighlighter for the temporary file
-        st.session_state[PDF_HIGHLIGHTER_KEY] = PDFHighlighter(st.session_state[PDF_HIGHLIGHTED_FILE_PATH_KEY])
+        file_path = st.session_state[PDF_HIGHLIGHTED_FILE_PATH_KEY]
+        st.session_state[PDF_HIGHLIGHTER_KEY] = PDFHighlighter(file_path)
         
         # if the file has previously been highlighted apply the highlights
         if check_for_previous_highlights(pdf_file_name):
@@ -25,7 +51,23 @@ def select_file_cb(pdf_file_name : str) -> None:
         st.session_state[PDF_SELECTED_KEY] = True
     else:
         st.error('ERROR')
-       
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+def back_cb() -> None:
+    
+    if st.session_state[ACTIVE_PAGE_NUMBER_KEY] > 0:
+        st.session_state[ACTIVE_PAGE_NUMBER_KEY] -= 1
+    else:
+        st.warning('At the beginning of the document')
+
+    update_displayed_pdf()
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    
+def next_cb() -> None:
+        
+    st.session_state[ACTIVE_PAGE_NUMBER_KEY] += 1
+    update_displayed_pdf() 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 def select_label_cb(selected_label : str) -> None:
@@ -38,6 +80,7 @@ def select_label_cb(selected_label : str) -> None:
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 def add_labelled_text_cb(selected_text : str) -> None:
+    
     # check that a label is selected if not show error msg
     if not st.session_state[ACTIVE_LABEL_KEY]:
         st.sidebar.error('No Label Selected.')
@@ -61,10 +104,13 @@ def add_labelled_text_cb(selected_text : str) -> None:
     # Save the labelled sentence for the session
     file_id = st.session_state[ACTIVE_PROXY_STATEMENT_KEY]['file-id']
     
-    st.session_state[ACTIVE_DATA_SET_KEY]['labelled-text'].append({'text': selected_text,
-                                                                   'label-id' : active_label['label-id'],
-                                                                   'file-id' : file_id,
-                                                                   'id' : generate_labelled_text_id()})                                            
+    page_number = st.session_state[ACTIVE_PAGE_NUMBER_KEY]
+        
+    st.session_state[ACTIVE_DATA_SET_KEY][DATA_SET_LABELLED_TEXT].append({  'text': selected_text,
+                                                                            'label-id' : active_label['label-id'],
+                                                                            'file-id' : file_id,
+                                                                            'id' : generate_labelled_text_id(),
+                                                                            'page_number' : page_number})                                            
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
@@ -91,10 +137,10 @@ def add_data_page():
         with st.sidebar.popover(f'Proxy Statements'):
             
             year_filter = st.radio('year',
-                                    options=['all','2015','2016','2017','2018','2019'])
+                                    options=['all','2015','2016','2017','2018','2019','labelled'],)
             
             selected_file = st.selectbox(   label='Available Proxy Statements',
-                                            options=get_proxy_statement_pdfs(year_filter),
+                                            options=get_proxy_statement_names(year_filter),
                                             index=None)    
             st.button(  'Select',
                         on_click=select_file_cb,
@@ -136,12 +182,27 @@ def add_data_page():
     # TODO: move this to another function
     # Main Page Widgets
     if st.session_state[PDF_SELECTED_KEY]:
-        st.markdown(f'Selected Proxy Statement: {get_active_proxy_statement_name()}')
+                
+        # TODO: find a better way to display this information to the user, maybe move this to the side bar
+        #st.markdown(f'Selected Proxy Statement: {get_active_proxy_statement_name()}')
+        # label_col,_ = st.columns([1,10])        
+        # label_col.color_picker(f'Active Label: {get_active_label_name()}',
+        #                         value=get_active_label_colour(),
+        #                         disabled=True)  
         
-        label_col,_ = st.columns([1,10])        
-        label_col.color_picker(f'Active Label: {get_active_label_name()}',
-                                value=get_active_label_colour(),
-                                disabled=True)        
+        _,prev_col,page_col, next_col,_ = st.columns([5,1,1,1,5])      
+        
+        
+        
+        prev_col.button( '<-',
+                        on_click=back_cb)
+                
+        page_col.text_input('Page #',
+                            value=st.session_state[ACTIVE_PAGE_NUMBER_KEY] + 1)                            
+                                     
+        next_col.button( '->',
+                        on_click=next_cb)
+        
         try:      
             with open(str(st.session_state[PDF_HIGHLIGHTED_FILE_PATH_KEY]) , 'rb') as pdf_file:
                 base64_pdf = base64.b64encode(pdf_file.read()).decode("utf-8")
@@ -149,5 +210,6 @@ def add_data_page():
                             <iframe src="data:application/pdf;base64,{base64_pdf}" width="800px" height="1000px" type="application/pdf"></iframe>
                             """
                 st.markdown(pdf_display, unsafe_allow_html=True)   
+       
         except FileNotFoundError:
             st.error(f'Directory not found: {str(st.session_state[PDF_HIGHLIGHTED_FILE_PATH_KEY])}') 
---
+++ webapp/pdf-splitter.py
@@ -0,0 +1,70 @@
+import os
+
+import fitz
+from pprint import pprint
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+def split_pdf(input_pdf, output_folder):
+    # Open the input PDF file
+    pdf = fitz.open(input_pdf)
+
+    # Iterate through each page of the PDF
+    for page_num in range(len(pdf)):
+        # Create a new PDF document for each page
+        new_pdf = fitz.open()
+        new_pdf.insert_pdf(pdf, from_page=page_num, to_page=page_num)
+
+        # Save the new PDF as a single-page PDF
+        
+        num_leading_zeros = 4 - len(str(page_num + 1))        
+        output_file = f"{output_folder}/page_{'0'*num_leading_zeros}{page_num + 1}.pdf"
+        new_pdf.save(output_file)
+        new_pdf.close()
+
+    # Close the input PDF file
+    pdf.close()
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+def get_pdf_files(directory):
+    pdf_files = []
+    for file in os.listdir(directory):
+        if file.endswith(".pdf"):
+            pdf_files.append(file)
+        else:
+            if os.path.isdir(f"{directory}/{file}"):
+                pdf_files.extend(get_pdf_files(f"{directory}/{file}"))
+  
+    return pdf_files
+  
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 
+
+def create_directory(directory):
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+        print(f"Directory '{directory}' created successfully!")
+    else:
+        print(f"Directory '{directory}' already exists.")
+
+   
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+def main():
+
+    SEARCH_DIRT = ".proxy-statements/pdfs/test"
+    pdf_files = get_pdf_files(SEARCH_DIRT)
+
+    for pdf_file in pdf_files:
+        input_pdf = f"{SEARCH_DIRT}/{pdf_file}"
+        output_folder = f"{SEARCH_DIRT}/{pdf_file.split('.')[0]}-split"
+        
+        create_directory(output_folder)        
+        
+        split_pdf(input_pdf, output_folder)
+
+    print("PDFs have been split successfully!")
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+if __name__ == "__main__":
+   main()
